19e86dd0725d4dd050e3af048f76531cb8102c14
Cédric VINCENT <cedric.vincent@st.com>
2012-03-13 10:18:38 +0100

    dummy: convert to memory API + enable the ARM version.

--- hw/dummy.c
+++ hw/dummy.c
@@ -1,32 +1,39 @@
 #include "qemu-common.h"
 #include "cpu-common.h"
 #include "targphys.h"
-#include "rwhandler.h"
+#include "memory.h"
+#include "exec-memory.h"
 #include "hw/loader.h"
 #include "hw/boards.h"
 
-static uint32_t dummy_content[64];
+static uint64_t dummy_content[64];
 const target_phys_addr_t dummy_address = 0xFFFF0000;
 
-static uint32_t dummy_read(ReadWriteHandler *handler, pcibus_t address, int length)
+static uint64_t dummy_read(void *opaque, target_phys_addr_t address, unsigned size)
 {
-    /* length is ignored.  */
     dummy_content[address] = dummy_content[address] ?: rand();
-    printf("@%" PRIu64 " -> %" PRIu32 " (%d)\n", (uint64_t)address, dummy_content[address], length);
+    printf("@%" PRIu64 " -> %" PRIu64 " (%u)\n", (uint64_t)address, dummy_content[address], size);
     return dummy_content[address];
 }
 
-static void dummy_write(ReadWriteHandler *handler, pcibus_t address, uint32_t value, int length)
+static void dummy_write(void *opaque, target_phys_addr_t address, uint64_t value, unsigned size)
 {
-    /* length is ignored.  */
-    printf("@%" PRIu64 " <- %" PRIu32 " (%d)\n", (uint64_t)address, value, length);
+    printf("@%" PRIu64 " <- %" PRIu64 " (%u)\n", (uint64_t)address, value, size);
     dummy_content[address] = value;
 }
 
-static ReadWriteHandler dummy_handler = {
+static const MemoryRegionOps dummy_handler = {
+    .read = dummy_read,
     .write = dummy_write,
-    .read  = dummy_read,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+    .impl = {
+        .min_access_size = 1,
+        .max_access_size = 8,
+    }
 };
+
+static MemoryRegion dummy_iomem;
+static MemoryRegion dummy_vram;
 
 static void dummy_init(ram_addr_t ram_size, const char *boot_device,
                        const char *kernel_filename, const char *kernel_cmdline,
@@ -42,24 +49,30 @@ static void dummy_init(ram_addr_t ram_size, const char *boot_device,
 
     /* Initialize the CPU and for the reset address.  */
     cpu_state = cpu_init(cpu_model);
-    cpu_state->pc = reset_address;
+
+#ifdef TARGET_ARM
+#  define PC regs[15]
+#else
+#  define PC pc
+#endif
+    cpu_state->PC = reset_address;
 
     /* Register the RAM at the reset address.  */
-    status = qemu_ram_alloc(NULL, "dummy.ram", ram_size);
-    cpu_register_physical_memory(reset_address, ram_size, status);
+    memory_region_init_ram(&dummy_vram, NULL, "dummy.vram", ram_size);
+    memory_region_add_subregion(get_system_memory(), reset_address, &dummy_vram);
 
     /* Initialize the RAM with the content of the program image.  Be
      * careful, load_image() doesn't check if the image fits the
      * RAM.  */
-    status = load_image(image_filename, qemu_get_ram_ptr(status));
+    status = load_image(image_filename, memory_region_get_ram_ptr(&dummy_vram));
     if (status < 0) {
         printf("Can't load '%s', aborting.\n", image_filename);
         exit(1);
     }
 
     /* Register the "dummy" handler. */
-    status = cpu_register_io_memory_simple(&dummy_handler, DEVICE_NATIVE_ENDIAN);
-    cpu_register_physical_memory(dummy_address, sizeof(dummy_content), status);
+    memory_region_init_io(&dummy_iomem, &dummy_handler, NULL, "dummy.iomem", sizeof(dummy_content));
+    memory_region_add_subregion(get_system_memory(), dummy_address, &dummy_iomem);
 }
 
 static QEMUMachine dummy_machine = {
